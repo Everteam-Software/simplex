48f7ac99e69be4ff47c6afea1ea7fb61c03ff448
Matthieu Riou <mriou@apache.org>
2009-07-31 16:58:28 -0700

    Additional JS test case for XML returned by function call.

--- src/test/resources/js-tests/outcalls.js
+++ src/test/resources/js-tests/outcalls.js
@@ -5,4 +5,9 @@ describe 'Javascript functions'
         var response = request(ROOT + "/fn-return-str", "POST", <wrap>you</wrap>);
         response.payload.toString().should.eql "Howdy you. Yeeha!"
     end
+
+    it "should work when manipulating returned complex elements"
+        var response = request(ROOT + "/row-subelmt-length", "POST", <wrap>you</wrap>);
+        response.payload.toString().should.eql "20"
+    end
 end
